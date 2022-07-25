bcf0ed7d5b4fe3e2637dfd595b9088e1f4f7f652
WlliamZhao <71152785+WilliamZhao1@users.noreply.github.com>
2022-07-25 05:46:48 -0400

    fixed rng error

--- rng.cc
+++ rng.cc
@@ -1,7 +1,7 @@
 #include "rng.h"
 #include <random>
 
-static int RNG::generateInt(int end, int start) { //there are errors here
+int RNG::generateInt(int end, int start) {
     std::default_random_engine rng{seed};
     std::uniform_int_distribution<> distr(start, end);
     return distr(rng);
