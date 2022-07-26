f6778d8ed712dfa16c581736fb63a8e7a3707fa8
lilyycao <https://lilyycao@github.com>
2022-07-26 04:00:28 -0400

    fixed rng

--- rng.cc
+++ rng.cc
@@ -1,7 +1,9 @@
 #include "rng.h"
+#include <chrono>
 #include <random>
 
 int RNG::generateInt(int end, int start) {
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
     std::default_random_engine rng{seed};
     std::uniform_int_distribution<> distr(start, end);
     return distr(rng);
--- rng.h
+++ rng.h
@@ -1,10 +1,9 @@
 #ifndef _RNG_H_
 #define _RNG_H_
-#include <chrono>
 
 class RNG {
     protected:
-        unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+        unsigned seed;
     public:
         int generateInt(int end, int start = 0);
 };
