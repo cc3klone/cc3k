52be202da87e4ed7d11e327ea3f7ade838aa01ce
lilyycao <https://lilyycao@github.com>
2022-07-26 08:17:41 -0400

    made rng a little more random

--- rng.cc
+++ rng.cc
@@ -4,7 +4,7 @@
 
 int RNG::generateInt(int end, int start) {
     unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-    std::default_random_engine rng{seed};
+    std::mt19937 rng{seed};
     std::uniform_int_distribution<> distr(start, end);
     return distr(rng);
 }
